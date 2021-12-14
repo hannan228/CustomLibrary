4eea90ac568525ad9e0e81e29a5838b2fd7faddc
hannan228 <abdulhannan1146@gmail.com>
2021-12-14 11:12:30 +0500

    Initial commit

---
+++ app/src/main/java/jav/app/customlibrary/MainActivity.java
@@ -0,0 +1,20 @@
+package jav.app.customlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void addition(View view) {
+
+    }
+}
---
+++ mylibrary/src/main/java/jav/app/mylibrary/Toaster.java
@@ -0,0 +1,21 @@
+package jav.app.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Toaster extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toaster);
+
+    }
+
+    public void addition(View view) {
+        
+    }
+
+}
